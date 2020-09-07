0947987fd312dc2a00e7024e1ebbe93ae4386ef4
lmhgithi <lmhfox@foxmail.com>
2020-09-07 16:19:28 +0800

    [lmh] display students list

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
+import StudentsList from './studentsList';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <body>
+          <StudentsList />
+        </body>
       </div>
     );
   }
---
+++ src/App/studentsList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import '../style/studentsList.scss';
+
+class studentsList extends Component {
+    state = {
+        data: [],
+    }
+
+    componentDidMount = () => {
+        URL = "http://localhost:8080/students"
+        fetch(URL, {
+            method:"GET",
+        }).then(Response => {
+            if (Response.status === 200) {
+                return Response.json();
+            }
+            else {
+                Promise.reject();
+            }
+        }).then(jsonData => {
+            this.setState({
+                data: jsonData,
+            })
+        })
+        
+    }
+
+  render() {
+    // console.log(this.state.data)
+    return (
+      <div className="students-list">
+        <h2>学员列表</h2>
+        <div className="students-list-content">
+            {
+                Object.keys(this.state.data).map(key => (
+                <p className='student' key={key}>
+                    {`${key}. ${this.state.data[key]}`}
+                </p>
+                ))
+            }
+        </div>
+            
+      </div>
+    );
+  }
+}
+
+export default studentsList;
